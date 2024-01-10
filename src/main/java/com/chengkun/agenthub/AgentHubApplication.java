e2a3c448c91d95f2c3f763a668cfb4c69a9f37f0
chengkun <357086686@qq.com>
2024-01-10 15:24:34 +0800

    init

---
+++ src/main/java/com/chengkun/agenthub/AgentHubApplication.java
@@ -0,0 +1,13 @@
+package com.chengkun.agenthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgentHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chengkun/agenthub/AgentHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chengkun.agenthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
